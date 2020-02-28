8c750d58a0422f874bf5800b95fa635f69725275
djunior97 <djjunior1997@hotmail.com>
2020-02-28 16:22:05 -0300

    chore: changed lint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -67,7 +67,7 @@ module.exports = {
     "class-methods-use-this": "off",
     "no-param-reassign": "off",
     "camelcase": "off",
-    "no-unused-vars": ["error", {"argsIgnorePattern": "next"}],
+    "no-unused-vars": ["warn", {"argsIgnorePattern": "next"}],
     "jest/no-disabled-tests": "warn",
     "jest/no-focused-tests": "error",
     "jest/no-identical-title": "error",
