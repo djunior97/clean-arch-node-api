b84ba47916ce8f5fcebf9fe4278242dffe15119b
djunior97 <djjunior1997@hotmail.com>
2020-02-28 10:17:52 -0300

    chore: add jest plugin to eslint and rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,9 +19,23 @@ module.exports = {
   },
   plugins: [
     '@typescript-eslint',
-    'prettier'
+    'prettier',
+    'jest'
   ],
   rules: {
+    "prettier/prettier": "error",
+    "class-methods-use-this": "off",
+    "no-param-reassign": "off",
+    "camelcase": "off",
     "no-unused-vars": ["error", {"argsIgnorePattern": "next"}],
+    "jest/no-disabled-tests": "warn",
+    "jest/no-focused-tests": "error",
+    "jest/no-identical-title": "error",
+    "jest/prefer-to-have-length": "warn",
+    "jest/valid-expect": "error",
+    "jest/no-undef": "off"
   },
+  env: {
+    "jest": true
+  }
 };
